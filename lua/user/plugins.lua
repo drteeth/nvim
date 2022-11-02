3a6e34d191c3cd2de613deb5141d696a015fcfa4
Benjamin Moss <drteeth@gmail.com>
2022-11-01 21:03:55 -0400

    disable snipets

--- lua/user/plugins.lua
+++ lua/user/plugins.lua
@@ -55,12 +55,12 @@ return packer.startup(function(use)
   use({ "hrsh7th/nvim-cmp" }) -- The completion plugin
   use({ "hrsh7th/cmp-buffer" }) -- buffer completions
   use({ "hrsh7th/cmp-path" }) -- path completions
-  use({ "saadparwaiz1/cmp_luasnip" }) -- snippet completions
+  -- use({ "saadparwaiz1/cmp_luasnip" }) -- snippet completions
   use({ "hrsh7th/cmp-nvim-lsp" })
   use({ "hrsh7th/cmp-nvim-lua" })
 
   -- snippets
-  use({ "L3MON4D3/LuaSnip" }) --snippet engine
+  -- use({ "L3MON4D3/LuaSnip" }) --snippet engine
   -- use({ "rafamadriz/friendly-snippets" }) -- a bunch of snippets to use
 
   -- LSP
